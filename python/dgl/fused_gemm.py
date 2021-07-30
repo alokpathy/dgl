Correct SpMM-based GEMM fusion (forward + backprop)

python/dgl/fused_gemm.py
@@ -121,8 +121,67 @@ class FusedGEMMSpMM(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, dC1, dC2):
-        print("in custom backward")
-        return None, None, None, None
+        a1, b1, a2, b2 = ctx.saved_tensors
+        arg_dC1 = to_dgl_nd(dC1)
+        arg_dC2 = to_dgl_nd(dC2)
+
+        grad_a1 = grad_b1 = None
+        grad_a2 = grad_b2 = None
+
+        ctx_cuda = F.context(b1)
+        dtype = F.dtype(b1)
+
+        if ctx.needs_input_grad[0] and ctx.needs_input_grad[2]:
+            b1 = b1.t()
+            b2 = b2.t()
+            b = torch.cat((b1, b2), dim=0)
+            grad_a = F.zeros((dC1.size(0) + dC2.size(0), b1.size(1)), dtype, ctx_cuda).cuda()
+
+            arg_b = to_dgl_nd(b)
+
+            arg_grad_a = to_dgl_nd_for_write(grad_a)
+
+            # _CAPI_DGLKernelFGEMM(arg_dC1, arg_b1, arg_grad_a1, \
+            #                         dC1.size(0), dC1.size(1), b1.size(1), \
+            #                         arg_dC2, arg_b2, arg_grad_a2, \
+            #                         dC2.size(0), dC2.size(1), b2.size(1))
+
+            _CAPI_DGLKernelFGEMMSpMM(arg_dC1, arg_b, arg_grad_a, \
+                                        dC1.size(0), dC1.size(1), b.size(1), \
+                                        arg_dC2, \
+                                        dC2.size(0), dC2.size(1), b.size(1))
+        elif ctx.needs_input_grad[0]:
+            grad_a1 = torch.matmul(dC1, b1.t()).cuda()
+        elif ctx.needs_input_grad[2]:
+            grad_a2 = torch.matmul(dC2, b2.t()).cuda()
+
+        if ctx.needs_input_grad[1] and ctx.needs_input_grad[3]:
+            a1 = a1.t()
+            a2 = a2.t()
+            dC = torch.cat((dC1, dC2), dim=0)
+            grad_b = F.zeros((a1.size(0) + a2.size(0), dC1.size(1)), dtype, ctx_cuda).cuda()
+
+            arg_a1 = to_dgl_nd(a1)
+            arg_a2 = to_dgl_nd(a2)
+            arg_dC = to_dgl_nd(dC)
+
+            arg_grad_b = to_dgl_nd_for_write(grad_b)
+
+            # _CAPI_DGLKernelFGEMM(arg_a1, arg_dC1, arg_grad_b1, \
+            #                         a1.size(0), a1.size(1), dC1.size(1), \
+            #                         arg_a2, arg_dC2, arg_grad_b2, \
+            #                         a2.size(0), a2.size(1), dC2.size(1))
+
+            _CAPI_DGLKernelFGEMMSpMM(arg_a1, arg_dC, arg_grad_b, \
+                                        a1.size(0), a1.size(1), dC.size(1), \
+                                        arg_a2, \
+                                        a2.size(0), a2.size(1), dC.size(1))
+        elif ctx.needs_input_grad[1]:
+            grad_b1 = torch.matmul(a1, dC1).cuda()
+        elif ctx.needs_input_grad[3]:
+            grad_b2 = torch.matmul(a2, dC2).cuda()
+
+        return grad_a1, grad_b1, grad_a2, grad_b2
 
 def fused_gemm(a1, b1, a2, b2):
     return FusedGEMM.apply(a1, b1, a2, b2)

python/dgl/nn/pytorch/conv/relgraphconv.py
@@ -43,7 +43,6 @@ def lowmem_matmul(h_t, weight, num_rels):
         # with th.cuda.amp.autocast():
         dim_count += h_t[etype].numel() + weight[etype].numel()
         result = th.matmul(h_t[etype], weight[etype])
-        print(f"etype: {etype} result: {result}")
         msg.append(result)
         bmm_time += stop_time(bmm_start, bmm_stop)
         th.cuda.nvtx.range_pop()
@@ -164,8 +163,6 @@ def lowmem_fgemm_spmm(h_t, weight, num_rels):
             # with th.cuda.amp.autocast():
             # result = th.matmul(h_t[etype], weight[etype])
             result1, result2 = fused_gemm_spmm(h_t[etype1], weight[etype1], h_t[etype2], weight[etype2])
-            print(f"etype1: {etype1} result1: {result1}")
-            print(f"etype2: {etype2} result2: {result2}")
             msg.append(result1)
             msg.append(result2)
             bmm_time += stop_time(bmm_start, bmm_stop)
@@ -177,7 +174,6 @@ def lowmem_fgemm_spmm(h_t, weight, num_rels):
             dim_count += h_t[etype].numel() + weight[etype].numel()
             # with th.cuda.amp.autocast():
             result = th.matmul(h_t[etype], weight[etype])
-            print(f"etype: {etype} result: {result}")
             msg.append(result)
             bmm_time += stop_time(bmm_start, bmm_stop)
             th.cuda.nvtx.range_pop()
@@ -409,13 +405,13 @@ class RelGraphConv(nn.Module):
             # msg, bmm_time, dim_count = lowmem_matmul(h_t, weight, self.num_rels)
 
             # fused gemm with spgemm
-            # msg, bmm_time, dim_count = lowmem_fgemm_spgemm(h_t, weight, self.num_rels)
+            msg, bmm_time, dim_count = lowmem_fgemm_spgemm(h_t, weight, self.num_rels)
 
             # fused gemm with larger gemm
             # msg, bmm_time, dim_count = lowmem_fgemm_gemm(h_t, weight, self.num_rels)
 
             # fused gemm with spmm
-            msg, bmm_time, dim_count = lowmem_fgemm_spmm(h_t, weight, self.num_rels)
+            # msg, bmm_time, dim_count = lowmem_fgemm_spmm(h_t, weight, self.num_rels)
 
             if timing:
                 print(f"bmm_time: {bmm_time} dim_count: {dim_count}", flush=True)